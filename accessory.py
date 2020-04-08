466fd54a4ce11778240ceb37000fc08010066159
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2020-04-08 02:12:10 +0200

    Add word_counter() to accessory

--- accessory.py
+++ accessory.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 # Name: accessory
-# Version: 0.1a1
+# Version: 0.1a2
 # Owner: Ruslan Korniichuk
 # E-mail: ruslan.korniichuk(at)gmail.com
+
+import re
+
+
+def word_counter(text):
+
+    total_words = 0
+
+    word_pattern = re.compile("(?:[a-zA-Z]+[-–’'`ʼ]?)*[a-zA-Z]+[’'`ʼ]?")
+    words = word_pattern.findall(text)
+    total_words = len(words)
+    return total_words
