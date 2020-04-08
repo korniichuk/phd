d9d64546f3f51df8c2e452645f5bb4b812fe6fb9
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2020-04-08 02:23:17 +0200

    Add filter_data() to accessory

--- accessory.py
+++ accessory.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 # Name: accessory
-# Version: 0.1a2
+# Version: 0.1a3
 # Owner: Ruslan Korniichuk
 # E-mail: ruslan.korniichuk(at)gmail.com
 
 import re
+
+from bs4 import BeautifulSoup
+
+
+def filter_data(data):
+
+    result = []
+
+    for text in data:
+        if text == '':
+            continue
+        total_words = word_counter(text)
+        if total_words < 2:
+            continue
+        soup = BeautifulSoup(text, 'lxml')
+        text = soup.get_text(separator=' ')
+        result.append(text)
+    return result
 
 
 def word_counter(text):
